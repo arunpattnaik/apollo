fd0d61be3fd3bf47a216230c7d427de45ff0c717
achyutbenz19 <achyut.benz@gmail.com>
2024-06-29 15:34:32 -0500

    tanstack react query

--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,19 @@ export default function Home() {
     e.preventDefault();
     try {
       const response = await fetch(`/api/generate`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ text: value }),
       });
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
       console.log(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
     setValue("");
   };
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export function Providers({ children, ...props }: any) {
-  return <NextUIProvider {...props}>{children}</NextUIProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider {...props}>{children}</NextUIProvider>
+    </QueryClientProvider>
+  );
 }
