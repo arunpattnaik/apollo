67c7f702ce0a8d64b5287a226ee44a19906e4bc7
achyutbenz19 <achyut.benz@gmail.com>
2024-07-02 16:05:16 -0500

    anthropic + reqs

--- api/client.py
+++ api/client.py
@@ -1,4 +1,8 @@
-import os
-from openai import AsyncOpenAI
-
-client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+from anthropic import AsyncAnthropic
+class AsyncAnthropicClient:
+    def __init__(self, api_key: str):
+        self.chat = AsyncAnthropic(api_key=api_key)
+        
+client = AsyncAnthropicClient(
+    api_key=os.getenv("ANTHROPIC_API_KEY"),
+)
